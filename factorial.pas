3f717ae00b323ca4412e319001c3fc848a3d2124
VANVS <38601016+VANVS@users.noreply.github.com>
2019-05-10 14:34:36 +0800

    源文件

---
+++ factorial.pas
@@ -0,0 +1,15 @@
+begin
+  integerr k;
+  integer k;
+  integer function F(n);
+    beginn\
+      integer n;
+      integer k;
+      integer n;
+      if n<=0 then F:=1
+      else F:=n*F(n-1)
+    end;
+  integer l;
+  read(m;
+  k:-=F(m;
+  write(k)
